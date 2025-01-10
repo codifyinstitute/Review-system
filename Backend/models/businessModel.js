1010afbafe91b22e07de2eb8e580c2768e4ee77c
HarshitRaul24 <harshitraul15@gmail.com>
2025-01-10 18:29:06 +0530

    Backend Created

---
+++ Backend/models/businessModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const businessModel = new mongoose.Schema({
+    BusinessId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    Name: {
+        type: String,
+        required: true
+    },
+    Link: {
+        type: String,
+        required: true
+    },
+    Review:[{
+        Description:{
+            type:String,
+            required:true
+        },
+        Status:{
+            type:String,
+            required:true
+        }
+    }]
+});
+
+const Business = mongoose.model("Business", businessModel);
+module.exports = Business;
